9bf58ea2fa965fd89a54d205e87c3f5780db929c
Lucas Santos Rodrigues <84406944+santoslucas@users.noreply.github.com>
2022-09-01 09:35:14 -0300

    Add files via upload

--- Teste.java
+++ Teste.java
@@ -3,19 +3,19 @@ import java.util.*;
 
 public class Teste {
 	public static void main (String[] args) {
-		int tamArvore[] = {1000, 2000, 3000, 4000, 5000,
-					6000, 7000, 8000, 9000};
+		int tamArvore[] = {10000, 20000, 30000, 40000, 50000,
+					60000, 70000, 80000, 90000, 100000};
 		
 		// Exercicio 4)a)
 		System.out.println("ORDENADOS:");
 		// Cria as 9 arvores com o tamanho (max[i]) variando de 1000 a 9000
-		for(int i = 0; i<9; i++) {
-		  	ArvoreBinaria arvoreTeste = new ArvoreBinaria ();
+		for(int i = 0; i<10; i++) {
+		  	ArvoreSBB arvoreTeste = new ArvoreSBB ();
 			Item item;
 			int chaves[] = new int[tamArvore[i]];
 			
 			for (int j = 0; j < tamArvore[i]; j++)
-			  chaves[j] = j;
+		      chaves[j] = j;
 			
 			for (int j = 0; j < tamArvore[i]; j++) { 
 			  item = new Item (chaves[j]);
@@ -25,7 +25,7 @@ public class Teste {
 			Item itemNaoExist = new Item(tamArvore[i]);
 			
 			long tempoAntesPesq = System.nanoTime();
-			arvoreTeste.pesquisa(itemNaoExist);	// procura itemNaoExistente
+			arvoreTeste.pesquisa(itemNaoExist); // pesquisa itemNaoExistente
 			long tempoDepoisPesq = System.nanoTime();
 			long tempoGasto = tempoDepoisPesq - tempoAntesPesq; // calcula o tempo gasto apenas no metodo pesquisa
 			
@@ -34,14 +34,14 @@ public class Teste {
 		
 		// Exercicio 4)b)
 		System.out.println("\nALEATORIOS:");
-		for(int i = 0; i<9; i++) {
+		for(int i = 0; i<10; i++) {
 			Random rand = new Random();
-		  	ArvoreBinaria arvoreTeste = new ArvoreBinaria ();
+		  	ArvoreSBB arvoreTeste = new ArvoreSBB ();
 			Item item;
 			int chaves[] = new int[tamArvore[i]];
 			
 			for (int j = 0; j < tamArvore[i]; j++)
-			  chaves[j] = rand.nextInt(); // insere numeros aleatorios
+		      chaves[j] = rand.nextInt(); // insere chaves aleatorios
 			
 			for (int j = 0; j < tamArvore[i]; j++) { 
 			  item = new Item (chaves[j]);
